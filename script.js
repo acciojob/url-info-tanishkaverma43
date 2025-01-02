6a7e40ce10e62fb09e5a961f7c6065a7f909f809
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 17:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-let url = "https://course.acciojob.com/journey";
+let url = window.location.href;
 let urllength = url.length();
 alert("The length of the URL is:" + urllength);
