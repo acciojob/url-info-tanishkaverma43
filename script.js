4c6cea046ed16ef86f6a61f2ab3227b253f30e33
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-02 17:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 let url = window.location.href;
 let urllength = url.length;
-alert("The length of the URL is:" + urllength);
+alert("The length of the URL is: " + urllength); 
